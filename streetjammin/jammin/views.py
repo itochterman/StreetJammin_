5b12c6ea1c10d41d41aa5261d7a1a2df275ab7c1
Isabella <irt221@nyu.edu>
2019-12-04 01:07:59 -0500

    initial configuration

---
+++ streetjammin/jammin/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'basic_templates/index.html', {'title': "StreetJammin", 'contributors': "By Yumi, Alice, Jamie and Bella"})
