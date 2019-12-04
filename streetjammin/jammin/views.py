1c22ddbcc19ee9c9fddd6a729b1342978287ad7f
Isabella <irt221@nyu.edu>
2019-12-04 13:57:55 -0500

    style up da login button

--- streetjammin/jammin/views.py
+++ streetjammin/jammin/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'basic_templates/index.html', {'title': "StreetJammin", 'contributors': "By Yumi, Alice, Jamie and Bella"})
+
+def login(request):
+    return render(request, 'basic_templates/registration/login')
--- streetjammin/streetjammin/urls.py
+++ streetjammin/streetjammin/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', views.login, name="login"),
+
 ]
