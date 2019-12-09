00cbafdadc08449d1270a719e028984b075c1e3c
Isabella <irt221@nyu.edu>
2019-12-09 18:31:18 -0500

    changes to organization + testing

--- streetjammin/jammin/views.py
+++ streetjammin/jammin/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+# from .models import Musician << this is how we import models 
 
 # Create your views here.
 def index(request):
+    # musician = Musician.objects.create(mid = 3, username = "bob") <<<< this is a test (it works)
     return render(request, 'basic_templates/index.html', {'title': "StreetJammin", 'contributors': "By Yumi, Alice, Jamie and Bella"})
 
 def login(request):
