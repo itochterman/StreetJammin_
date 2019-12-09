6de4e24a5d4393f571cb3945cb6b51758c6ddcb6
Isabella <irt221@nyu.edu>
2019-12-09 17:05:17 -0500

    Revert "Merge pull request #1 from itochterman/db_branch"
    This reverts commit 88b56c74996a801d9633ea4c109e08b85620c5ae, reversing
    changes made to bcf0a9192c04e1d5a926549cb935475f50f4fe24.
    
    removing bad merge

--- streetjammin/streetjammin/db/jammin_db.sql
+++
@@ -1,6 +0,0 @@
-/* Create our database */
-CREATE DATABASE jammin_db CHARACTER SET utf8;
-
-/* Setup permissions for the server */
-CREATE USER 'main_user'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL ON scalica.* TO 'appserver'@'localhost';
--- streetjammin/streetjammin/jammin/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class JamminConfig(AppConfig):
-    name = 'jammin'
