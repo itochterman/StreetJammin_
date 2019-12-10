b24e87105105b788be3c003a6d70c878c43ebf8a
unknown <you@example.com>
2019-12-10 01:11:34 -0500

    linking together

--- streetjammin/streetjammin/urls.py
+++ streetjammin/streetjammin/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('upload/', views.uploadSongs, name="upload"),
+    path('list/', views.mySongs, name="list"),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/login/', views.login, name="login"),
