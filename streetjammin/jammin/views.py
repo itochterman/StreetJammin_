b78f74e1b7bcfd900dd8d69a43180aa7dd6e9bb0
unknown <you@example.com>
2019-12-10 01:03:51 -0500

    views please work part 4

--- streetjammin/jammin/views.py
+++ streetjammin/jammin/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.shortcuts import redirect
+from .models import Songs
 # from .models import Musician << this is how we import models
 
 # Create your views here.
